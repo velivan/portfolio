05c8c1f206d82ff8c408fab6c79164b7dbd7985a
ivan <vic86@ya.ru>
2022-07-25 14:38:35 +0300

    first commit

---
+++ src/component/langue/langue.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Langue (){
+    return(
+        <select name="langue">
+        <option value="EN" key=""><p>EN</p></option>
+        <option value="UA" key=""><p>UA</p></option>
+        </select>
+    )
+}
+export default Langue
---
+++ src/component/nav/nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Langue from "../langue/langue";
+
+const Nav = () =>{
+    return(
+        <div>
+        <h1>Nav Bar</h1>
+        <Langue />
+        </div>
+        
+    )
+}
+export default Nav
