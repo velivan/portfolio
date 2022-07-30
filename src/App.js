b90e4bb60ac722b99f1068c749bc5cf1240e85a4
velivan <32300223+velivan@users.noreply.github.com>
2022-07-30 18:13:22 +0300

    Save

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import './App.css';
-import Nav from './component/header/nav/Nav';
-import langue from './component/langue/CheckLangue';
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
-import Pagetwo from './component/Pages/Pagetwo';
-import Pageone from './component/Pages/Pageone';
-import Homepage from './component/Pages/Homepage';
-import Chat from './component/Chat/Chat';
-import dataUser from './API/dataUser.json';
-import dataMsg from './API/dataMsg.json';
-import todolist from './API/todolist.json';
-import Todo from './component/Todo/Todo';
+import "./App.css";
+import Nav from "./component/header/nav/Nav";
+import langue from "./component/langue/CheckLangue";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Pagetwo from "./component/Pages/Pagetwo";
+import Pageone from "./component/Pages/Pageone";
+import Homepage from "./component/Pages/Homepage";
+import Chat from "./component/Chat/Chat";
+import dataUser from "./API/dataUser.json";
+import dataMsg from "./API/dataMsg.json";
+import todolist from "./API/todolist.json";
+import Todo from "./component/Todo/Todo";
 
 
 function App(props) {
 
   return (
- <BrowserRouter>
- <div className="App">
-    <Nav />
-    
-    <h1>{langue()  ? "EN": "UA"}</h1>
-      <Routes>
-        <Route path="/" element={<Homepage />}/>
-        <Route path="/Pageone" element={<Pageone />}/>
-        <Route path="/Pagetwo" element={<Pagetwo />}/>
-        <Route path="/Todo" element={<Todo todolist={todolist} myList={todolist} />}/>
-        <Route path="/Chat" element={<Chat dataUser={dataUser} dataMsg={dataMsg}/>}/>
-      </Routes>
-    </div>
- </BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+
+        <h1>{langue() ? "EN" : "UA"}</h1>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Pageone" element={<Pageone />} />
+          <Route path="/Pagetwo" element={<Pagetwo />} />
+          <Route
+            path="/Todo"
+            element={<Todo todolist={todolist} myList={todolist} />}
+          />
+          <Route
+            path="/Chat"
+            element={<Chat dataUser={dataUser} dataMsg={dataMsg} />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/redux/redux.js
@@ -0,0 +1,5 @@
+import   * as st   from 'redux';
+
+const store = st.createStore;
+
+export default store;
